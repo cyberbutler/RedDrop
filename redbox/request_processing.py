410d973454b84f832c8e6a65355294e8a46b50e0
cyberbutler <cyber0x5fbutler@gmail.com>
2022-03-17 10:01:32 -0400

    fixed logger messages

--- redbox/request_processing.py
+++ redbox/request_processing.py
@@ -2,7 +2,7 @@ from redbox.logger import logger
 from redbox.config import config
 from redbox.processors import processFromList
 
-def processRequestParameter(field, value, processing_list=[], decryption_list=[]) -> dict:
+def processRequestParameter(field, value, processing_list=[]) -> dict:
     try:
         processedData = str(value).encode("utf-8")
     except AttributeError as e:
@@ -13,7 +13,7 @@ def processRequestParameter(field, value, processing_list=[], decryption_list=[]
         processedData, processing_list = processFromList(processedData, processing_list)
 
     except RecursionError:
-        logger.error("The payload could not be processed automatically. It may be getting recognized repeatedly by a data processor. You should force the coding parameters.")
+        logger.error("The payload could not be processed automatically. It may be getting recognized repeatedly by a data processor. You should force the processing parameters.")
 
     try:
         processedData = processedData.decode("utf-8") if processedData else None
@@ -27,7 +27,7 @@ def processRequestParameter(field, value, processing_list=[], decryption_list=[]
                 f'Detected: Process Order: {processing_list}'
             ))
         else:
-            logger.error(f'There was an issue when attempting to decode the processedData. Is the data encoded or encrypted properly? Decode Order: {processing_list} - Decrypt Order: {decryption_list}')
+            logger.error(f'There was an issue when attempting to decode the processedData. Is the data encoded or encrypted properly?')
 
         processedData = None
     
