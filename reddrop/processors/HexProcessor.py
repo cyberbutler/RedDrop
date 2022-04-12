9b50727c62139e567908d0246d0bc9004926ebe4
cyberbutler <cyber0x5fbutler@gmail.com>
2022-04-11 20:12:50 -0400

    updated regex for hex processor to match on entire payload

--- reddrop/processors/HexProcessor.py
+++ reddrop/processors/HexProcessor.py
@@ -12,5 +12,5 @@ class HexProcessor(BaseProcessor):
 
     def validate(self, encoded:bytes):
         encoded = encoded.replace(b'\n', b'')
-        regex = re.compile(b'([0-9a-f]{2,})|([0-9A-F]{2,})')
+        regex = re.compile(b'^([0-9a-fA-F]{2,})$')
         return bool(regex.match(encoded)) and len(encoded) % 2 == 0
